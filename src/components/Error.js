08bba98da8547e3ca2536ba631bc1d4d653ebcc9
Kamil Stróżyk <strozykkamil@gmail.com>
2021-02-27 14:30:28 +0100

    Add simple routing

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Error extends Component {
+    render() {
+        return (
+            <h1>Error</h1>
+        );
+    }
+}
+
+export default Error;
