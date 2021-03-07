0750af2a70b94327ae99e0a90faeefaed82f357b
KamilStrozyk <strozykkamil@gmail.com>
2021-03-07 16:11:43 +0100

    add sidebar content

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar'
-import Sidebar from './Sidebar'
+import Sidebar from './Sidebar/Sidebar'
 export const Layout = ({ children }) => {
     return (
         <>
-
-            <div class="flex flex-col h-screen justify-between">
-                <main class ="bg-gray-50 lg:ml-60 mt-16 lg:w-5/6">
+            <div class="flex flex-col h-screen justify-between bg-gray-50">
+                <main class="lg:ml-60 mt-16 lg:w-5/6">
                     <div class="container mx-auto px-4">{children}
                     </div>
-                    </main>
+                </main>
                 <Navbar />
                 <Sidebar />
             </div>
---
+++ src/components/Layout/Sidebar/Overview.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Overview extends Component {
+    render() {
+        return (
+           <blockquote >
+            I'm junior .Net Developer and IT student at WSB. Engineer at Poznan University of Technology. There will be something about my passions etc. It's just a sample now.
+           </blockquote > 
+        );
+    }
+}
+
+export default Overview;
